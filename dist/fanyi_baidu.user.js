99002284abcb1cb2e5adf9d1d4bd9ed3c9c96217
Hunlongyu <hunlongyu@gmail.com>
2024-01-24 11:16:53 +0800

    feat: 添加净网卫士，百度翻译

---
+++ dist/fanyi_baidu.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name               『净网卫士』百度翻译
+// @name:zh-CN         『净网卫士』百度翻译
+// @namespace          userscript.hunlongyu.dev
+// @version            0.2.0
+// @author             Hunlongyu
+// @description        Block ads on Baidu's translation interface, streamline pages, and optimize layout.
+// @description:zh-CN  页面精简，去除广告，只保留主要功能的部分。
+// @icon               https://i.loli.net/2019/04/22/5cbd720718fdb.png
+// @supportURL         https://github.com/Hunlongyu/userscript/issues
+// @match              *://fanyi.baidu.com/*
+// @grant              GM_addStyle
+// @grant              GM_log
+// @run-at             document-start
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  var _GM_addStyle = /* @__PURE__ */ (() => typeof GM_addStyle != "undefined" ? GM_addStyle : void 0)();
+  var _GM_log = /* @__PURE__ */ (() => typeof GM_log != "undefined" ? GM_log : void 0)();
+  const css = `
+    #header{display:none !important;}
+    .trans-domain-btn{display: none !important;}
+    .ai-trans-btn{display: none !important;}
+    .manual-trans-btn{display: none !important;}
+    .collection-btn{display: none !important;}
+    .op-trans-fb{display: none !important;}
+    #app-read{display: none !important;}
+    .footer{display: none !important;}
+    .note-expand-btn{display: none !important;}
+    .trans-other-right{display: none;}
+    .app-guide{display: none !important;}
+    .desktop-guide-wrapper{display: none !important;}
+  `;
+  try {
+    _GM_addStyle(css);
+  } catch (e) {
+    _GM_log(new Error("GM_addStyle stopped working！"));
+  }
+
+})();
--- vite.config.js
+++ vite.config.js
@@ -8,26 +8,20 @@ export default defineConfig({
       entry: 'src/main.js',
       userscript: {
         name: {
-          '': '『净网卫士』网盘净化',
-          'zh-CN': '『净网卫士』网盘净化'
+          '': '『净网卫士』百度翻译',
+          'zh-CN': '『净网卫士』百度翻译'
         },
         description: {
-          '': 'Block advertisements, pop ups, promotions, shopping malls, etc',
-          'zh-CN': '屏蔽广告、弹框、推广、商场等'
+          '': "Block ads on Baidu's translation interface, streamline pages, and optimize layout.",
+          'zh-CN': '页面精简，去除广告，只保留主要功能的部分。'
         },
         icon: 'https://i.loli.net/2019/04/22/5cbd720718fdb.png',
         namespace: 'userscript.hunlongyu.dev',
         match: [
-          'https://www.123pan.com/*',
-          'https://*.lanzoul.com/*',
-          'https://*.lanzouo.com/*',
-          'https://pan.quark.cn/*',
-          'https://pan.baidu.com/*',
-          'https://www.aliyundrive.com/*',
-          'http://ct.ghpym.com/*'
+          '*://fanyi.baidu.com/*'
         ],
-        grant: ['GM_addStyle'],
-        'run-at': 'document-start'
+        'run-at': 'document-start',
+        supportURL: 'https://github.com/Hunlongyu/userscript/issues'
       }
     })
   ]
