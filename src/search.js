fb25149f7d9efab976fec0609494a7b3e6092728
Hunlongyu <hunlongyu@gmail.com>
2024-09-24 17:56:25 +0800

    feat: 修改描述文件，优化 search 交互

--- src/search.js
+++ src/search.js
@@ -1,9 +1,13 @@
+import Swal from "sweetalert2";
 import {
+	add_filter_words,
 	get_filter_words_arr,
 	hasAuthor,
 	hasKeyWords,
 	hasRepo,
 } from "./utils.js";
+
+import { create_add_filter_ui } from "./ui.js";
 
 export function search_results_filter(dom) {
 	const target = dom.querySelector('[data-testid="results-list"]');
@@ -15,6 +19,7 @@ export function search_results_filter(dom) {
 	for (let i = 0; i < divs.length; ++i) {
 		const div = divs[i];
 		check_search_item(div);
+		avatar_add_handle(div);
 	}
 
 	show_filter_number();
@@ -68,4 +73,54 @@ function show_filter_number() {
 
 	const search_num = window.search_nums;
 	num.textContent = `过滤数量：${search_num}`;
+}
+
+function avatar_add_handle(dom) {
+	const avatar = dom.querySelector("[data-testid=github-avatar]");
+	if (!avatar) return;
+	avatar.style.cursor = "pointer";
+	avatar.addEventListener("click", async () => {
+		const res = await create_add_filter_ui();
+
+		const item = dom.querySelector(".search-match");
+		if (!item) return;
+		const text = item.textContent;
+		const vec = text.trim().split("/");
+		const author = vec[0];
+		const repo = vec[1];
+		const authorAndRepo = `${author}/${repo}`;
+
+		let flag = -1;
+		if (res === "author") {
+			flag = add_filter_words("AuthorWords", author);
+		}
+		if (res === "repo") {
+			flag = add_filter_words("RepoWords", authorAndRepo);
+		}
+		if (flag === -1) return;
+		const toast = Swal.mixin({
+			toast: true,
+			position: "top-end",
+			showConfirmButton: false,
+			timer: 3000,
+			timerProgressBar: true,
+		});
+		if (flag) {
+			toast.fire({
+				icon: "success",
+				title: "添加成功",
+			});
+			if (res === "author") {
+				document.location.reload();
+			}
+			if (res === "repo") {
+				check_search_item(dom);
+			}
+		} else {
+			toast.fire({
+				icon: "warning",
+				title: "添加失败, 已存在",
+			});
+		}
+	});
 }
