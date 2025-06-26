d0ad3bfa667f6c0c91d5973dac88095e3f4aae3d
Hunlongyu <Hunlongyu@users.noreply.github.com>
2025-06-26 15:27:28 +0800

    Add user script for Vite vanilla starter and update main.js styles
    - Created a new user script file for the Vite vanilla starter project.
    - Added CSS rules to main.js to hide specific elements on the page.
    - Included additional styles for better UI presentation.

--- src/main.js
+++ src/main.js
@@ -3,15 +3,19 @@ import { GM_addStyle, GM_log } from '$'
 
 const css = `
     .KxVKmLZM{display:none !important;}
+    ._m6jE1Mj{display:none !important;}
     .RAbZsoLs{display:none !important;}
     .YGx8668_{display:none !important;}
-    .UMjeGiEI{display:none !important;}
-    .qJU3axmS{display:none !important;}
-    .sF3Yx_p0{display:none !important;}
-    .LSOa73BQ{display:none !important;}
-    .qoOttmGv{display:none !important;}
+    .h5JHRvHF{display:none !important;}
+    .jygLSCYa{display:none !important;}
+    .LxF9kyWA{display:none !important;}
+    .Sp5qqVdz{display:none !important;}
     .XS9zPMly{display:none !important;}
-    ._m6jE1Mj{display:none !important;}
+    .sF3Yx_p0{display:none !important;}
+    .qJU3axmS{display:none !important;}
+    .dfAvRK2O{display:none !important;}
+    .EUENIlUT{height: 100%; !important;}
+    .UMjeGiEI{display:none !important;}
   `
 try {
   GM_addStyle(css)
